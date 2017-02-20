18795022d9452cba8571b1ca5cc57251d735c369
yanggb <muyuqiu001@163.com>
2017-02-21 00:44:11 +0800

    #1 能够实现删除与添加的功能

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent } from './app.component';
@@ -10,10 +11,16 @@ import { HeroService } from './hero.service'
 import { DashboardComponent } from './dashboard.component'
 import { AppRoutingModule }     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
